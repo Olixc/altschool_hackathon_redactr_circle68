7cf4829a54b1a3bde2b9743297e9989db001be94
Noibi Abdulrasaq O <108674374+Olixc@users.noreply.github.com>
2022-07-21 17:41:33 +0000

    Completed the footer

---
+++ js/index.js
@@ -0,0 +1,30 @@
+const menuBtn = document.querySelector('.header__bar');
+const hamburger = document.querySelector('.header__bar--burger');
+const nav = document.querySelector('.header__nav');
+const menuNav = document.querySelector('.header__listContainer');
+const navItems = document.querySelectorAll('.header__listItem');
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', function(){
+    console.log("clicked");
+    toggleMenu()
+});
+
+function toggleMenu() {
+  if(!showMenu) {
+    hamburger.classList.add('open');
+    nav.classList.add('open');
+    menuNav.classList.add('open');
+    navItems.forEach(item => item.classList.add('open'));
+
+    showMenu = true;
+  } else {
+    hamburger.classList.remove('open');
+    nav.classList.remove('open');
+    menuNav.classList.remove('open');
+    navItems.forEach(item => item.classList.remove('open'));
+
+    showMenu = false;
+  }
+}
